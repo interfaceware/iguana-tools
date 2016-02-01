1b7b15bf2d223da7a88a9a113d419d8e96d3e4bb
iNTERFACEWARE Inc <admin@interfaceware.com>
2016-02-01 16:32:34 -0500

    Delete MapADT.lua

--- XML_Techniques-To-yvUZJ4Wh4o2exC/MapADT.lua
+++
@@ -1,7 +0,0 @@
-
--- Mapping ADT message
-function MapADT(Msg, X)
-   -- Notice how the PID segment is in a different spot
-   -- from the lab messsage - see MapLab
-   MapPatient(Msg.PID, X.message.patient)   
-end
