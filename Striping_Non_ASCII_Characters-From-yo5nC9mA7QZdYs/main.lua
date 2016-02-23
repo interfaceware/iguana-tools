7296b74475b13ae23a00789c5955d5e64e8f858a
admin <paul.gannon@interfaceware.com>
2016-02-23 16:33:21 +0000

    Export: Second commit of this channel - first commit did not quite work.

---
+++ Striping_Non_ASCII_Characters-From-yo5nC9mA7QZdYs/main.lua
@@ -0,0 +1,10 @@
+binstrip = require 'binstrip'
+
+-- Sometimes you need to remove characters that have a value of 128 or above.  This module does this.
+
+function main()
+   local BadString = "This is Fred who has some\244\135 issues."
+   trace(BadString)
+   local GoodString = binstrip.strip(BadString)
+   trace(GoodString)
+end
