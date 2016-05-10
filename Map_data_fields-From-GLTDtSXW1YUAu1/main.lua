5cc5b5cd87de386267b7271cae1f469ab3285f03
admin <paul.gannon@interfaceware.com>
2016-05-10 20:54:22 +0000

    Export: codemap now has help.

--- Map_data_fields-From-GLTDtSXW1YUAu1/main.lua
+++ Map_data_fields-From-GLTDtSXW1YUAu1/main.lua
@@ -3,12 +3,7 @@
 
 -- http://help.interfaceware.com/v6/codemap-example
 
-codemap = require 'codemap'
-
-function main()
-   MapCodes()
-   CheckSets()
-end
+local codemap = require 'codemap' 
 
 -- We set up the code map
 local SexCodeMap = codemap.map({
@@ -19,10 +14,11 @@ local SexCodeMap = codemap.map({
       W='Female'
    },'other')
 
-function MapCodes()
+local function MapCodes()
    -- This shows mapping functionality
    local V = 'M'
    local W = 'W'
+   trace(V, W)
    local A = SexCodeMap[V]
    local B = SexCodeMap[W]
    trace(A)
@@ -33,7 +29,7 @@ end
 -- if something is a member of it.
 local AmigoSet = codemap.set{"Fred", "Jim", "Harry"}
 
-function CheckSets()
+local function CheckSets()
    local Name1 = "Fred"
    local Name2 = "Mary"
    if (AmigoSet[Name1]) then
@@ -43,4 +39,10 @@ function CheckSets()
    if (not AmigoSet[Name2]) then
       trace(Name2 .. " is not one the three amigos!")
    end   
+end
+
+function main()
+   trace(AmigoSet)
+   MapCodes()
+   CheckSets()
 end
--- shared/codemap.lua
+++ shared/codemap.lua
@@ -1,9 +1,12 @@
--- This code map library has a number of useful helper routines for doing mapping and checking codes etc.
+-- This code map module has a number of useful helper 
+-- routines for doing mapping and checking codes etc.
+
+-- http://help.interfaceware.com/v6/codemap-example
  
 local codemap = {}
  
 -- Make a set
--- MySet = codemap.set{"Fred", "Bruce", "Jim"}
+-- local MySet = codemap.set{"Fred", "Bruce", "Jim"}
 -- if MySet[Msg.PID[3][1][1]] then print('One of our three amigos') end
 function codemap.set(a)
    local m = {}
@@ -12,8 +15,9 @@ function codemap.set(a)
 end
  
 -- Create a map the advantage of this function is that it will take a tree node without
--- needing to convert it to a string.  Example of usage
--- SexMap = codemap.map({'M'='Male', 'm'='Male', 'F'='Female', 'f'='Female'},'defaultValue')
+-- needing to convert it to a string. 
+-- Example of usage:
+-- local SexMap = codemap.map({'M'='Male', 'm'='Male', 'F'='Female', 'f'='Female'},'default')
 function codemap.map(m, default)
    return setmetatable(m, {
          __index = function(m,k)
@@ -24,5 +28,93 @@ function codemap.map(m, default)
             return default
          end})
 end
- 
+
+-- help for functions
+
+local HELP_DEF=[[{
+"Desc": "Make a code set for membership checking, by supplying one or more key values <u>table</u>.
+Membership is checked against the table keys.<br><br>
+<b>Note</b>: The value (in each key/value pair) is set to a placeholder value of 'true'.",
+"Returns": [
+{
+"Desc": "Code set used for membership checking <u>table</u>."
+}
+],
+"SummaryLine": "Make a code set for membership checking.",
+"SeeAlso": [
+{
+"Title": "Map data fields",
+"Link": "http://help.interfaceware.com/v6/codemap-example"
+},
+{
+"Title": "Module for mapping data fields",
+"Link": "https://github.com/interfaceware/iguana-tools/blob/master/shared/codemap.lua"
+}
+],
+"Title": "codemap.set",
+"Usage": "codemap.set{&lt;key&gt; [, &lt;key&gt; ,...]}",
+"Parameters": [
+{
+"Codeset": {
+"Desc": "A table of key values <u>table</u>. "
+}
+}
+],
+"Examples": [
+"<pre>local AmigoSet = codemap.set{'Fred', 'Jim', 'Harry'}
+local isMember = AmigoSet['Harry']   -- true
+local isMember = AmigoSet['William'] -- false</pre>"
+],
+"ParameterTable": true
+}]]
+
+help.set{input_function=codemap.set, help_data=json.parse{data=HELP_DEF}}
+
+-- Create a map the advantage of this function is that it will take a tree node without
+-- needing to convert it to a string.  Example of usage
+-- SexMap = codemap.map({'M'='Male', 'm'='Male', 'F'='Female', 'f'='Female'},'default')
+ local HELP_DEF=[[{
+"Desc": "Make a codemap table, to use for mapping one codeset to another.",
+"Returns": [
+{
+"Desc": "A codemap table for codeset mapping <u>table</u>."
+}
+],
+"SummaryLine": "Make a codemap.",
+"SeeAlso": [
+{
+"Title": "Map data fields",
+"Link": "http://help.interfaceware.com/v6/codemap-example"
+},
+{
+"Title": "Module for mapping data fields",
+"Link": "https://github.com/interfaceware/iguana-tools/blob/master/shared/codemap.lua"
+}
+],
+"Title": "codemap.map",
+"Usage": "codemap.map({&lt;key&gt; = &lt;value&gt; [, &lt;key&gt; = &lt;value&gt; ,...]} [, &lt;default&gt])",
+"Parameters": [
+{
+"Codemap": {
+"Desc": "A table of key/value pairs to map from/to <u>table</u>."
+}
+},
+{
+"Default": {
+"Desc": "Default value to use if there is no matching key <u>string</u>.","Opt":true
+}
+}
+],
+"Examples": [
+"<pre>local SexMap = codemap.map({'M'='Male',  'F'='Female', 'W'='Female'},'other')
+local Mapping = SexCodeMap['M'] -- maps to 'Male'
+local Mapping = SexCodeMap['W'] -- maps to 'Female'
+local Mapping = SexCodeMap['f'] -- maps to 'other'
+</pre>"
+],
+"ParameterTable": false
+}]]
+
+help.set{input_function=codemap.map, help_data=json.parse{data=HELP_DEF}}
+
 return codemap
