fad289de9afe53609953078a96cab6049e7c7ed2
admin <paul.gannon@interfaceware.com>
2016-05-18 18:59:47 +0000

    Export: Updated with help and more documentation.

--- Reformat_Report-To-fG8bMCBCqMELNK/main.lua
+++ Reformat_Report-To-fG8bMCBCqMELNK/main.lua
@@ -2,19 +2,29 @@
 -- The goal is to split text into lines of equal length to make it easier to
 -- display in a downstream system.
 
+-- http://help.interfaceware.com/v6/reformat-report
+
 -- It's a good idea to expand out the reports before and after to see them.
 
+-- notice how we use require add the function to the "string" global namespace
 string.formatText = require 'string.formatText'
 
+local MAXLENGTH = 80   -- the maximum length, in characters
+
 function main(Before)
-   local FirstReport = Before:split("\r")
-   trace("We start with "..#FirstReport.." lines")
-   local MAXLENGTH = 80 -- the maximum length, in characters
-   local SplitTextArray = Before:formatText(MAXLENGTH, SplitTextArray)
-   trace(SplitTextArray)
+
+   -- count and display the number of lines in the initial report
+   local FirstReport = Before:split('\r')          -- split report and
+   trace("We start with "..#FirstReport.." lines") -- show line count
+   
+   -- split the report into a table of smaller lines of 
+   -- whole words with each line no longer than MAXLENGTH
+   local SplitTextArray = Before:formatText(MAXLENGTH)
+
+   -- concatenate the table with smaller lines into a single string
+   -- using a separator of "\n"
    local After = table.concat(SplitTextArray, "\n")
-   trace(After)
+
+   -- display count of lines for the reformatted report
    trace("And end with "..#SplitTextArray.." lines of shorter length.")   
 end
-
-
--- shared/string/formatText.lua
+++ shared/string/formatText.lua
@@ -1,21 +1,58 @@
-local function formatText(s, M, RS)
+-- This module splits text into lines of equal length 
+-- often used to make a report easier to display in a downstream system.
+
+-- http://help.interfaceware.com/v6/reformat-report
+
+local function formatText(s, M)
    local function lengthofWord(word)
       if word == '\n' or word == '\r' or word == '\t' then return #word + 1 end
       return #word
    end
-   local function RSindex(i)
+   local function RSindex(RS, i)
       if (#tostring(RS[i])) > (M - countOfWord - 2) then return i+1  end    
       return i
    end
    local i, position = 1, 0
-   RS = {[1]=''} 
+   local RS = {[1]=''} 
    for word in s:rxmatch('(\\w+.|\\s)') do
       countOfWord = lengthofWord(word)
-      i = RSindex(i) 
+      i = RSindex(RS, i) 
       if RS[i] == nil then RS[i] = '' end
       RS[i] = RS[i]..word 
    end
    return RS
 end
 
+local Help = {
+   Title="string.formatText",
+   Usage="string.formatText(text, maxlength)",
+   Desc=[[Breaks a block of text into several smaller lines each ending in a whole word, 
+with a maximum length of the maxlength parameter.<br><br><b>Note:</b> The block of text 
+may contain multiple lines.]],
+   ParameterTable=false,
+   Parameters={
+      {text={Desc="Block of text to be formatted <u>string</u>."}},
+      {maxlength={Desc="Maximum line length allowed <u>integer</u>."}}
+   },
+   Returns={
+      {Desc="One or more lines of text as specified <u>table</u>."}
+   },
+   Examples={[[-- reformat "Report" into shorter lines 
+local MAXLENGTH = 80
+local SplitTextArray = Report:formatText(MAXLENGTH)]]},
+   SeeAlso={
+      {
+         Title="Reformat Report",
+         Link="http://help.interfaceware.com/v6/reformat-report"
+      },
+      {
+         Title="Source code for the string.formatText.lua module on github",
+         Link="https://github.com/interfaceware/iguana-tools/blob/master/shared/string/formatText.lua"
+      }
+   },
+}
+
+help.set{input_function=formatText, help_data=Help}
+
 return formatText
+
